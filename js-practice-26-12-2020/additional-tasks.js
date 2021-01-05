13de50aa1f64550d433c2240fa02c86a806a6fe0
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-05 16:32:19 +0100

    Update additional-tasks.js

--- js-practice-26-12-2020/additional-tasks.js
+++ js-practice-26-12-2020/additional-tasks.js
@@ -159,3 +159,21 @@
 // console.log(string.reverse('abcde'));
 // console.log(string.ucFirst('abcde'));
 // console.log(string.ucWords('every hunter whants to know'));
+
+// #4.
+// Реализуйте класс Validator, который будет проверять строки. К примеру, у него будет метод isEmail параметром принимает строку и проверяет, является ли она корректным емейлом или нет. Если является - возвращает true, если не является - то false. Кроме того, класс будет иметь следующие методы: метод isDomain для проверки домена, метод isDate для проверки даты и метод isPhone для проверки телефона:
+// var validator = new Validator();
+// console.log(validator.isEmail('phphtml@mail.ru'));
+// console.log(validator.isDomain('phphtml.net'));
+// console.log(validator.isDate('12.05.2020'));
+// console.log(validator.isPhone('+375 (29) 817-68-92')); //тут можете формат своей страны
+
+
+class Validator {
+	isEmail(str) {
+		return /[a-z0-9]@[a-z0-9].[a-z]/.test(str); 
+	}
+
+}
+const validator = new Validator;
+console.log(validator.isEmail('1111@fsdfsd'));
