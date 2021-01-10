ed1a252de1ff4ab9c187bf381de112fc8da4f350
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-10 17:36:00 +0100

    Update 01-basic-js.js

--- js-freecodecamp/01-basic-js.js
+++ js-freecodecamp/01-basic-js.js
@@ -50,15 +50,99 @@
 
 // console.log(8, updateRecords(collection, 1245, "albumTitle", "Riptide")); //albumTitle should be Riptide
 
-// -------> 
+// -------> Replace Loops using Recursion
 
-var myArray = [];
+// function sum(arr, n) {
+// 	if(n<=0) {
+// 			return 0;
+// 		} else {
+// 			return sum(arr, n-1)+arr[n-1];
+// 		}
+// }
 
-// Only change code below this line
-var i = 5;
-while (i!==-1) {
-    myArray.push(i);
-    i--;
-}
+// -------> Basic JavaScript: Profile Lookup
 
-console.log(myArray);
+// var contacts = [
+//     {
+//         "firstName": "Akira",
+//         "lastName": "Laine",
+//         "number": "0543236543",
+//         "likes": ["Pizza", "Coding", "Brownie Points"]
+//     },
+//     {
+//         "firstName": "Harry",
+//         "lastName": "Potter",
+//         "number": "0994372684",
+//         "likes": ["Hogwarts", "Magic", "Hagrid"]
+//     },
+//     {
+//         "firstName": "Sherlock",
+//         "lastName": "Holmes",
+//         "number": "0487345643",
+//         "likes": ["Intriguing Cases", "Violin"]
+//     },
+//     {
+//         "firstName": "Kristian",
+//         "lastName": "Vos",
+//         "number": "unknown",
+//         "likes": ["JavaScript", "Gaming", "Foxes"]
+//     }
+// ];
+
+// function lookUpProfile(name, prop){
+// // Only change code below this line
+// 	for (const contact of contacts) {
+// 		if (contact.firstName === name) {
+// 			if (prop in contact) {
+// 				return contact[prop];
+// 			} else {
+// 				return 'No such property'
+// 			}
+// 		}
+// 	}
+// 	return 'no such name';
+// // Only change code above this line
+// }
+
+// console.log(1, lookUpProfile("Akira", "likes"));
+// console.log(2, lookUpProfile("Kristian", "lastName")); //"Vos"
+// console.log(3, lookUpProfile("Sherlock", "likes")); // should return ["Intriguing Cases", "Violin"]
+// console.log(4, lookUpProfile("Harry", "likes")); // should return an array
+// console.log(5, lookUpProfile("Bob", "number"));// should return "No such contact"
+// console.log(6, lookUpProfile("Bob", "potato"));// should return "No such contact"
+// console.log(7, lookUpProfile("Akira", "address"));// should return "No such property"
+
+//------> 
+// function convertToInteger(str) {
+// return parseInt(str, 2);
+// }
+
+// console.log(convertToInteger("10011"));
+
+
+// ------> recursion
+// function countdown(n){
+//   if(n<1){
+//   return []
+//   } else{
+// 		const reversedArray = countdown(n-1);
+// 		reversedArray.unshift(n);
+// 		return reversedArray;
+//   }
+// }
+
+// console.log(countdown(5));
+
+// ------> 
+function rangeOfNumbers(startNum, endNum) {
+
+		if (endNum < startNum) {
+			return [];
+		} else {
+			const arr = rangeOfNumbers(startNum, endNum - 1);
+			arr.push(endNum);
+			return arr;
+	}
+
+};
+console.log(rangeOfNumbers(6, 9));
