498e632d9cfbefd2e1056b94a57ff0d1b93366af
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-12 16:28:12 +0100

    Create 03-regular-expressions.js

---
+++ js-freecodecamp/03-regular-expressions.js
@@ -0,0 +1,37 @@
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString); // Change this line
+// console.log(result);
+
+
+//------->
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/ig; // Change this line
+// let result = quoteSample.match(vowelRegex); // Change this line
+// console.log(result);
+
+//------->
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/ig; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+// console.log(result);
+
+//------->
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/ig; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+//------->
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^aeiou0-9]/ig; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+//------->
+// let difficultSpelling = "Misssissippi";
+// let myRegex = /s+/ig; // Change this line
+// let result = difficultSpelling.match(myRegex);
+// console.log(result);
+
+//------->
