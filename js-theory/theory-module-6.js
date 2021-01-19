bfd1cfb7ce3914860b89975c2510b22f918784eb
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-19 12:21:46 +0100

    Update theory-module-6.js

--- js-theory/theory-module-6.js
+++ js-theory/theory-module-6.js
@@ -371,13 +371,13 @@
 
 // console.log(users.sort(sortByActiveDays));
 
-const players = [
-	{id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false},
-	{id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true},
-	{id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true},
-	{id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false},
-	{id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true},
-]
+// const players = [
+// 	{id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false},
+// 	{id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true},
+// 	{id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true},
+// 	{id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false},
+// 	{id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true},
+// ]
 
 // const playerNames = players.map(function (player) {
 // 	return {
@@ -387,5 +387,18 @@ const players = [
 
 // console.log(playerNames);
 
-const hardcorePlayers = players.filter((player) => player.timePlayed > 200);
-console.table(hardcorePlayers);
+// const hardcorePlayers = players.filter((player) => player.timePlayed > 200);
+// console.table(hardcorePlayers);
+
+/* 
+*
+* -------------> Arrow functions <------------------
+*
+*/
+
+const foo = function () {
+	console.log(arguments);
+	const arr = [...arguments];
+	return arr
+}
+console.log(foo(1,2,3,4));
