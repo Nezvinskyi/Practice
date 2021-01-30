cad22ae17b7dcaffce6cac4964f6bc3e63f83f7b
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-30 01:44:10 +0100

    adds lazy load workshop

---
+++ js-workshops/ws-lazy-load-m8.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.feed img');
+
+const options = {
+	rootMargin: '100px',
+}
+
+const onEntry = (entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			const image = entry.target;
+			const src = image.dataset.lazy;
+			image.src = src;
+			image.classList.add('appear');
+			observer.unobserve(image);
+	 }
+ })
+}
+
+const io = new IntersectionObserver(onEntry, options)
+
+images.forEach(image => io.observe(image));
