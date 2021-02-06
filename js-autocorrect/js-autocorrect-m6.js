9b2edecb0fd084f01de35c8ea1dc1a22b5510f1f
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-06 12:02:38 +0100

    adds tasks with autocorrection for module 6

--- js-autocorrect/js-autocorrect-m6.js
+++ js-autocorrect/js-autocorrect-m6.js
@@ -209,33 +209,234 @@ const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
 // Task-37
 
 const books = [
-  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
-  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
-  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
-  { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
-  { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 },
+  {
+    index: 0,
+    title: 'Последнее королевство',
+    author: 'Бернард Корнуэлл',
+    rating: 8.38,
+  },
+  {
+    index: 1,
+    title: 'На берегу спокойных вод',
+    author: 'Роберт Шекли',
+    rating: 8.51,
+  },
+  {
+    index: 2,
+    title: 'Сон смешного человека',
+    author: 'Федор Достоевский',
+    rating: 7.75,
+  },
+  { index: 3, title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+  { index: 4, title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 },
 ];
 // Пиши код ниже этой строки
+console.log('task-37');
 
-const sortedByAuthorName = books.sort(
-  (firstBook, secondBook) => firstBook.author - secondBook.author,
+const sortedByAuthorName = books.sort((firstBook, secondBook) =>
+  firstBook.author.localeCompare(secondBook.author),
 );
-// console.log(sortedByAuthorName);
+console.log(sortedByAuthorName);
 
-const sortedByReversedAuthorName = books.sort(
-  (firstBook, secondBook) => secondBook.author - firstBook.author,
+const sortedByReversedAuthorName = books.sort((firstBook, secondBook) =>
+  secondBook.author.localeCompare(firstBook.author),
 );
+console.log(sortedByReversedAuthorName);
 
 const sortedByAscendingRating = books.sort(
   (firstBook, secondBook) => firstBook.rating - secondBook.rating,
 );
+console.log(sortedByAscendingRating);
 
-// const sortedByDescentingRating = books.sort((firstBook, secondBook) =>
-//   secondBook.rating.localeCompare(firstBook.rating),
-// );
+const sortedByDescentingRating = books.sort(
+  (firstBook, secondBook) => secondBook.rating - firstBook.rating,
+);
+console.log(sortedByDescentingRating);
 
-// Task-10
-// Task-10
-// Task-10
-// Task-10
+// Task-38
+const users38 = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+const sortByAscendingBalance = users => {
+  const sortedByBalance = [...users].sort(
+    (firstUser, secondUser) => firstUser.balance - secondUser.balance,
+  );
+  return sortedByBalance;
+};
+console.log('Task-38');
+console.table(sortByAscendingBalance(users38));
+
+// Task-41
+const books41 = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+  { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 },
+];
+const MIN_BOOK_RATING = 8;
+// Пиши код ниже этой строки
+
+const names = [...books41]
+  .filter(book => book.rating >= MIN_BOOK_RATING)
+  .map(book => book.author)
+  .sort();
+console.log('Task-41');
+console.log(names);
+
+// Task-43
+
+const users43 = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: [
+      'Jacklyn Lucas',
+      'Linda Chapman',
+      'Adrian Cross',
+      'Solomon Fokes',
+    ],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+const getSortedFriends = users => {
+  return [...users]
+    .flatMap(user => user.friends)
+    .sort()
+    .filter((user, index, array) => index === array.indexOf(user));
+};
+console.log('Task-43');
+console.log(getSortedFriends(users43));
+
+// Task-44
+
+const getTotalBalanceByGender = (users, gender) => {
+  return [...users]
+    .filter(user => user.gender === gender)
+    .reduce((acc, currUser) => acc + currUser.balance, 0);
+};
+
+console.log('Task-44');
+console.log(getTotalBalanceByGender(users43, 'male'));
+
 // Task-10
