4a6030aa884d11c39e4c68b405d7db38bf3ec9f2
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-20 19:44:42 +0100

    adds sandbox practice

---
+++ js-theory/sandbox.js
@@ -0,0 +1,12 @@
+// for (let i = 1; i <= 10; i++) {
+//   function foo(x) {
+//     setTimeout(() => {
+//       console.log(x);
+//     }, 0);
+//   }
+//   foo(i);
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+console.log();
