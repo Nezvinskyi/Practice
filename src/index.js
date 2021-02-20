4c2fb68c5912115efb91e0ecdcc8c43cd7de2436
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-20 19:46:22 +0100

    Adds webpack files and practice

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/dist/basiclightbox.min.css';
-import './styles.css';
-
-const modalTemplate = document.querySelector('#modal');
-const instance = basicLightbox.create(modalTemplate);
-
-instance.show();
