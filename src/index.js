6dd3131eba8b29347c8d0eb7b47d45cfaa13f1cd
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-03-06 08:55:48 +0100

    adds pagination files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import './css/common.css';
 
-import pokemonCardTpl from '../src/templates/pokemon-card.hbs';
-import API from './js/api-service';
-import getRefs from './js/get-refs';
+/*
+ * https://newsapi.org/
+ * 4330ebfabc654a6992c2aa792f3173a3
+ * http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
+ */
 
-const refs = getRefs();
+const options = {
+  headers: {
+    Authorization: 'd082869b7f79460ea045ec72443d6f3f',
+  },
+};
 
-refs.searchForm.addEventListener('submit', onSearch);
-
-function onSearch(e) {
-  e.preventDefault();
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
-
-  API.fetchPokemon(searchQuery).then(renderPokemonCard).catch(onFetchError);
-}
-
-function renderPokemonCard(pokemon) {
-  const markup = pokemonCardTpl(pokemon);
-  refs.cardContainer.innerHTML = markup;
-}
-
-function onFetchError(error) {
-  alert('Wooopppssss.... :-(');
-}
-
-// ======+++ rest
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=1000')
-  .then(r => r.json())
-  .then(console.log);
-
-fetch(
-  'https://pixabay.com/api/?key=20542752-0490219c09a6da4a08c0fa17e&q=yellow+flowers&image_type=photo',
-)
+fetch('https://newsapi.org/v2/everything?q=imac&pageSize=5&page=3', options)
   .then(r => r.json())
   .then(console.log);
